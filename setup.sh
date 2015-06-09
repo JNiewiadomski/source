25d2e05463e9f9ce73fa2e6ad66de55311abadbc
JNiewiadomski <Jack.Niewiadomski@vorne.com>
2015-06-09 08:23:29 -0500

    Simplify setup.

--- setup.sh
+++ setup.sh
@@ -14,40 +14,14 @@ for SRC in `find ${RCFILES} -type f \( ! -name "*.swp" \)`
 do
     DST=~/.`basename ${SRC}`;
 
-    # If file is symbolic link we're done.
-    if [ -h ${DST} ];
+    # If the destination file exists remove it.
+    if [ -f ${DST} ];
     then
-        ;
-
-    # File already exists, but not a symbolic link.
-    elif [ -f ${DST} ];
-    then
-        diff --brief ${DST} ${SRC};
-
-        # File is different in home directory.
-        if [ $? -eq 1 ];
-        then
-            diff ${DST} ${SRC};
-            echo "Remove original and create symbolic link (y/n)?";
-            read YN;
-            if test "${YN}" = "y";
-            then
-                rm ${DST};
-                ln -s ${DIR}/${SRC} ${DST};
-            fi
-
-        # File is the same. Offer to remove and create symbolic link.
-        else
-            echo "No difference in '${DST}' and '${SRC}'. Removing original and creating symbolic link.";
-            rm ${DST};
-            ln -s ${DIR}/${SRC} ${DST};
-        fi
-
-    # File does not exist.
-    else
-        echo "File '${DST}' does not exist. Creating symbolic link.";
-        ln -s ${DIR}/${SRC} ${DST};
+        rm ${DST};
     fi
+
+    # Create symbolic link to file.
+    ln -s ${DIR}/${SRC} ${DST};
 done
 
 ###################################################################################################
