c942910edfe5739c43a5461d41094d328cf4eab6
JNiewiadomski <Jack.Niewiadomski@vorne.com>
2020-01-13 09:30:38 -0600

    Clean up

--- setup.sh
+++ setup.sh
@@ -1,40 +1,54 @@
 #!/bin/bash
 
-###################################################################################################
-#
-# Creates a soft link to all files in the home/ directory to the ~ directory.
-#
-###################################################################################################
+create_soft_links() {
+    local -r HOME_FILES="$(dirname "${0}")/home";
 
-HOME_FILES="$(dirname "${0}")/home";
-echo "Creating soft links to files in ${HOME_FILES}"
+    echo "Creating soft links to files in ${HOME_FILES}"
 
-for SRC in "${HOME_FILES}"/{.[!.],}*
-do
-    DST=~/$(basename "${SRC}");
+    local SRC
+    local DST
 
-    # Remove existing symbolic links.
-    if [ -h "${DST}" ];
-    then
-        unlink "${DST}";
+    for SRC in "${HOME_FILES}"/{.[!.],}*
+    do
+        DST=~/$(basename "${SRC}");
+
+        # Remove existing symbolic links.
+        if [ -h "${DST}" ];
+        then
+            unlink "${DST}";
+        fi
+
+        # Remove existing destination files.
+        if [ -f "${DST}" ];
+        then
+            rm -f "${DST}";
+        fi
+
+        # Create symbolic link to file.
+        echo "${DST} -> ${SRC}"
+        ln -s "${SRC}" "${DST}";
+    done
+}
+
+create_vim_directories() {
+    # Create directories used by VIM as specified in .vimrc file.
+    mkdir -p ~/.vim/backup
+    mkdir -p ~/.vim/swap
+    mkdir -p ~/.vim/undo
+}
+
+set_login_shell() {
+    if sudo where chsh 2> /dev/null ; then
+        echo "Setting bash as login shell."
+        sudo chsh -s /bin/bash ${USER}
+    else
+        echo
+        echo "Unable to set bash as login shell for ${USER}: chsh not found."
+        echo "Manually edit /etc/passwd."
+        return 1
     fi
+}
 
-    # Remove existing destination files.
-    if [ -f "${DST}" ];
-    then
-        rm -f "${DST}";
-    fi
-
-    # Create symbolic link to file.
-    echo "${DST} -> ${SRC}"
-    ln -s "${SRC}" "${DST}";
-done
-
-###################################################################################################
-#
-# Create directories used by VIM as specified in .vimrc file.
-#
-###################################################################################################
-mkdir -p ~/.vim/backup
-mkdir -p ~/.vim/swap
-mkdir -p ~/.vim/undo
+create_soft_links
+create_vim_directories
+set_login_shell
